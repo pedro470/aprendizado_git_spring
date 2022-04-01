9822b40dd44397cdaafda9d8ebc1bae3e7841145
Pedro Correia de Araújo Moura <77297069+pedro470@users.noreply.github.com>
2022-04-01 12:58:04 -0300

    commit inicial springboot

---
+++ src/main/java/com/aprender/primeiro_teste/PrimeiroTesteApplication.java
@@ -0,0 +1,13 @@
+package com.aprender.primeiro_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aprender/primeiro_teste/PrimeiroTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprender.primeiro_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
