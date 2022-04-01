b14b8767b08ea9e5547dd55f176d6309aa56c6a6
Pedro Correia de Araújo Moura <77297069+pedro470@users.noreply.github.com>
2022-04-01 14:22:03 -0300

    adicionando a anotação feign client

--- src/main/java/com/aprender/primeiro_teste/PrimeiroTesteApplication.java
+++ src/main/java/com/aprender/primeiro_teste/PrimeiroTesteApplication.java
@@ -2,8 +2,10 @@ package com.aprender.primeiro_teste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PrimeiroTesteApplication {
 
 	public static void main(String[] args) {
