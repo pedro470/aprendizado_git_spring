40b1490f6a9bb05bdee66818a57abe13b6726d9d
Pedro Correia de Araújo Moura <77297069+pedro470@users.noreply.github.com>
2022-04-02 21:59:49 -0300

    arquivo falho

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_user(id,nome,email) VALUES (unhex(replace(uuid(),'-','')),'Pedro', 'pedro@uol.br')
+INSERT INTO tb_user(id,nome,email) VALUES (unhex(replace(uuid(),'-','')),'Bruno', 'bruno@yahoo.br')
+    
